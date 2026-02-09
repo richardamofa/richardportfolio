4b8afd41ab642068b4b8591dac856bc60f266db1
richie-codesxp <amofarichard101@gmail.com>
2026-02-09 03:07:27 +0000

    BTP

--- script.js
+++ script.js
@@ -132,7 +132,7 @@
   const backToTop = document.getElementById("backToTop");
 
   window.addEventListener("scroll", () => {
-    if (window.scrollY > 400) {
+    if (window.scrollY > 200) {
       backToTop.classList.add("show");
     } else {
       backToTop.classList.remove("show");
